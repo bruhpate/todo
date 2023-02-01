69d1668eece9c640f14383a24c644fc80d1fd4a0
bruhpate <pietropatelli05@gmail.com>
2023-02-01 20:12:59 +0100

    added main, printList, printVector(private), splitArgs

---
+++ todo.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+vector<string> splitArgs(int argc, char *argv[]){ 
+    vector<string> arguments;
+    for(int i = 1; i < argc; i++){
+        arguments.push_back(argv[i]);
+    }
+    return arguments;
+}
+
+void printVector(size_t t, vector<string> v){
+    for(int i = 0; i < t; i++){
+        cout << v[i] << ", ";
+    }
+    cout << endl;
+}
+
+int printList(string doc){
+    ifstream file(doc);
+    if(!file.is_open()){
+        cerr << "[Errore]   Impossible to open \".todo\" file" << endl;
+        return 1;
+    }
+    string line;
+    while(getline(file, line)){
+        cout << line << endl;
+    }
+    file.close();
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    const auto arguments = splitArgs(argc, argv);
+    const auto n_arguments = arguments.size();
+    printList(".todo");
+    return 0;
+}
